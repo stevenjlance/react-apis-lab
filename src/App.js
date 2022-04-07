9e0b490f4e98ff34b28b00840b925176e8da5dc4
stevenjlance <44912347+stevenjlance@users.noreply.github.com>
2022-04-07 13:48:21 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Squirrel from "./Components/Squirrel";
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      data: []
+    };
   }
-  
+
   // add a componentDidMount lifecycle method to fetch data from the API
+  componentDidMount = () => {
+    fetch(
+      "https://data.cityofnewyork.us/resource/vfnx-vebw.json?$$app_token=2y6eroq5UDtsqbdRlo8SOA9rt&running=true"
+    )
+      .then((data) => data.json())
+      .then((data) => {
+        this.setState({ data: data });
+        console.log(this.state.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
   render() {
     return (
       <div className="App">
         <h1>2018 NYC Squirrel Survey Data</h1>
+        <h2>Number of Observations: {this.state.data.length}</h2>
+        {this.state.data.map((squirrel) => (
+          <Squirrel data={squirrel} />
+        ))}
+        {/* <Squirrel data={this.state.data} /> */}
       </div>
     );
   }
--- src/Components/Squirrel.js
+++ src/Components/Squirrel.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React from "react";
 
 function Squirrel(props) {
+  let fur = props.data.primary_fur_color;
+  if (fur === "Cinnamon") {
+    fur = "#C58C66 ";
+  } else if (fur === "Gray") {
+    fur = "#808080";
+  }
+  let dot = {
+    height: "25px",
+    width: "25px",
+    backgroundColor: fur,
+    borderRadius: "50%",
+    display: "inline-block"
+  };
+  let symbol = "";
+  if (props.data.age === "Juvenile") {
+    symbol = "🍼";
+  }
+
   return (
     <div className="Squirrel">
-      <h3>SQUIRREL CARD!</h3>
+      <h3 className="title">
+        ID: {props.data.unique_squirrel_id} {symbol}
+      </h3>
+      <div style={dot}></div>
     </div>
   );
 }
